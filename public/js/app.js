ffc11cb5fc12c725add361d89befb424ac75e051
vovamvt <mebvest@gmail.com>
2023-07-14 12:47:16 +0300

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('js is running');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
